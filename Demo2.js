8edf53876bf1327c8c449417f4726e6b796c27b8
Ranjini1210 <gpranjinisri2k7@gmail.com>
2026-01-31 22:42:35 +0530

    Add files via upload

---
+++ Demo2.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First JavaScript Program</h1>
+
+<script>
+  
+  document.write('Hello JS');
+  let a = "Raghavi";
+let b = 20;
+
+console.log("Name:", a);
+console.log("Age:", b);
+console.log(`My name is {a} and I am {b} years old.`);
+</script>
+
+<p> JavaScript.</p>
+
+</body>
+</html>// This is a single-line comment.
+
+/*
+  This is a multi-line comment.
+  It can span multiple lines
+  and is useful for longer descriptions or commenting out blocks of code.
+*/
+
+// Another single-line comment
